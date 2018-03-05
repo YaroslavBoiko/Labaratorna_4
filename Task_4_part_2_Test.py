b89b8b46b010dda2522648a4c32a4ab99176aec7
YaroslavBoiko <35149052+YaroslavBoiko@users.noreply.github.com>
2018-03-05 22:13:07 +0200

    Create Task_4_part_2_Test.py

---
+++ Task_4_part_2_Test.py
@@ -0,0 +1,9 @@
+import Task_4_part_1 as l
+
+agent = l.Agent()
+agent.add_property()
+agent.add_property()
+agent.display_properties()
+agent.count_properties()
+agent.relation()
+print("Count of properties is ",agent.count)
